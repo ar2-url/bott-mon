c648144640f3bf8ed8eb940bb71fb39ba16c8d7c
ar2-url <57313716+ar2-url@users.noreply.github.com>
2021-05-01 09:50:42 +0100

    Create bot-mon.swift

---
+++ bot-mon.swift
@@ -0,0 +1,33 @@
+var numBottles: Int = 99
+while numBottles > 0 {
+  print("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk!")
+  print("You take one down and pass it around…")
+  numBottles -= 1;
+  print("\(numBottles) bottles of milk on the wall!")
+}
+print("\(numBottles) bottles of milk on the wall,") 
+print("\(numBottles) bottles of milk!") 
+print("Go to the store and buy some more,") 
+print("99 bottles of milk on the wall!")
+
+
+
+//*********************************************************
+
+var numMonkeys: Int = 5
+
+// Lyrics will print while numMonkeys is greater than 1
+while numMonkeys > 1 {
+  print ("\(numMonkeys) little monkeys jumping on the bed.")
+  print ("One fell off and bumped their head!")
+  print ("Mama called the doctor and the doctor said")
+  print ("'No more monkeys jumping on the bed!'\n")
+  numMonkeys -= 1
+}
+
+// Print the final lyrics
+print ("\(numMonkeys) little monkey jumping on the bed.")
+print ("They fell off and bumped their head!")
+print ("Mama called the doctor and the doctor said")
+print ("'Put those monkeys straight to bed!'")
+
